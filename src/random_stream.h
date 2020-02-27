1623c70c2a36eda514e5d5c2f3d233941aeb36f7
Kevin Kuehler <keur@xcf.berkeley.edu>
2020-02-27 00:56:40 -0800

    Add random_stream class
    This class creates a stream of deterministic pseudorandom data given a
    key K and will clamp random numbers into a uniform number of buckets.

---
+++ src/random_stream.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2020, The Hina Authors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+#include <sodium.h>
+
+#define KEY_LEN randombytes_SEEDBYTES
+#define NUM_RAND_BYTES 128
+
+class StreamingDPRNG {
+public:
+    typedef uint8_t byte;
+    typedef std::array<byte, NUM_RAND_BYTES> random_pool;
+    typedef std::array<byte, KEY_LEN> arr_key;
+private:
+    int pos;
+    uint32_t buckets;
+    random_pool buf;
+    arr_key key;
+
+    uint32_t maybe_reseed_and_get_random();
+public:
+    StreamingDPRNG(uint8_t key[KEY_LEN], uint32_t buckets);
+    uint32_t get_random();
+};
